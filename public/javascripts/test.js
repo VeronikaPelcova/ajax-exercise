0d82e3b91b7f7377738337a639870a70c97bff68
Veronika Pelcová <nika.pelcova@gmail.com>
2015-11-15 16:33:23 +0100

    Clicking on title triggers dialog with detail

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,1 +1,27 @@
-console.log("Loaded JavaScript.");
+$(".title").on("click", function() {
+    $(this).parent().children(".detail").clone().dialog({
+        buttons: [
+            {
+                text: "Ok",
+                icons: {
+                    primary: "ui-icon-heart"
+                },
+                click: function() {
+                    $( this ).dialog( "close" );
+                }
+            }
+        ]
+    });
+});
+
+$( "#dialog" ).dialog({
+    dialogClass: "no-close",
+    buttons: [
+        {
+            text: "OK",
+            click: function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    ]
+});
